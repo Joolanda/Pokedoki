fd1bd49b0aed2777765a64d33a099cc3b52894f7
Joolanda <50619917+Joolanda@users.noreply.github.com>
2019-11-11 15:21:41 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*!
+ * Here comes all Javascript
+ * Pokedoki App
+ * Doki Doki! You clicked the button
+ * Date: 2019-11-11
+ */
+var myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
