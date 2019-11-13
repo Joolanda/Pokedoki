1f39e9370f555bad5698c5b6fa2e7e45a178f504
Joolanda <50619917+Joolanda@users.noreply.github.com>
2019-11-13 23:27:11 +0100

    clean script and add new variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,64 @@
 /*!
  * Here comes all Javascript
  * Pokedoki App
- * Doki Doki! You clicked the button
  * Date: 2019-11-11
  */
- alert('Hello Jon & rest of the world!');
 
- var myFavoriteFood = 'Lasagna';
- document.write(myFavoriteFood);
+ // This array will become the repository of Pokémon to display in myapplication
+var repository = [
+  PokemonNames,
+  PokemonHeight,
+  PokemonTypes,
+];
+
+// array of pokemon names
+var PokemonNames = [
+  "Absol",
+  "Luxray",
+  "Pikachu",
+  "Milotic",
+  "Eevee",
+];
+
+// array of pokemon types, use array of strings
+var PokemonTypes = [
+  'water',
+  'electric',
+  'dark',
+  'normal'
+];
+
+// array of pokemon heights, use array of strings
+var PokemonHeights = [
+  "3'11/"",
+  "4'07/"",
+  "1'04/"",
+  "20'04/"",
+  "1'00/"",
+];
+
+// asigning valuea to the variables
+var Absol = {
+  PokemonHeight: "3'11/"",
+  PokemonTypes: 'dark',
+};
+
+var Luxray = {
+  PokemonHeight: "4'07/"",
+  PokemonTypes: 'electric',
+};
+
+var Pikachu = {
+  PokemonHeight: "1'04/"",
+  PokemonTypes: 'electric',
+};
+
+var Milotic = {
+  PokemonHeight: "20'04/"",
+  PokemonTypes: 'water',
+};
+
+var Eevee = {
+  PokemonHeight: "1'00/"",
+  PokemonTypes: 'normal',
+};
