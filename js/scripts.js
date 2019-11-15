9d7fd71f390528657c8d1731324a4d53a7d68198
Joolanda <50619917+Joolanda@users.noreply.github.com>
2019-11-15 15:18:54 +0100

    reducing spec variables

--- js/scripts.js
+++ js/scripts.js
@@ -6,65 +6,33 @@
 
  // This array will become the repository of Pokémon to display in myapplication
 var repository = [
-  pokemonName,
-  pokemonHeight,
-  pokemonType,
+  {
+    pokemonName: 'Absol',
+    pokemonHeight: "3'11\"",
+    pokemonType: 'dark',
+  };
+
+  {
+    pokemonName:'Luxray',
+    pokemonHeight: "4'07\"",
+    pokemonType: 'electric',
+  };
+
+  {
+    pokemonName:'Pikachu',
+    pokemonHeight: "1'04\"",
+    pokemonType: 'electric',
+  };
+
+  {
+    pokemonName: 'Milotic',
+    pokemonHeight: "20'04\"",
+    pokemonType: 'water',
+  };
+
+  {
+    pokemonName:'Eevee',
+    pokemonHeight: "1'00\"",
+    pokemonType: 'normal',
+  }
 ];
-
-// array of pokemon names
-var pokemonName = [
-  'Absol',
-  'Luxray',
-  'Pikachu',
-  'Milotic',
-  'Eevee',
-];
-
-// array of pokemon types, use array of strings
-var pokemonType = [
-  'water',
-  'electric',
-  'dark',
-  'normal'
-];
-
-// array of pokemon heights, use array of strings
-var pokemonHeight = [
-  "3'11\"",
-  "4'07\"",
-  "1'04\"",
-  "20'04\"",
-  "1'00\"",
-];
-
-
-// defining the pokemons without external input
-var absol = {
-  pokemonName: 'Absol',
-  pokemonHeight: "3'11\"",
-  pokemonType: 'dark',
-};
-
-var luxray = {
-  pokemonName:'Luxray',
-  pokemonHeight: "4'07\"",
-  pokemonType: 'electric',
-};
-
-var pikachu = {
-  pokemonName:'Pikachu',
-  pokemonHeight: "1'04\"",
-  pokemonType: 'electric',
-};
-
-var milotic = {
-  pokemonName: 'Milotic',
-  pokemonHeight: "20'04\"",
-  pokemonType: 'water',
-};
-
-var eevee = {
-  pokemonName:'Eevee',
-  pokemonHeight: "1'00\"",
-  pokemonType: 'normal',
-};
