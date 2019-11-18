7710d1efd491ac8bb43d7d71dd170b33e0077111
Joolanda <50619917+Joolanda@users.noreply.github.com>
2019-11-18 11:29:21 +0100

    each object seperate line

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,37 @@
  // This array will become the repository of Pokémon to display in myapplication
 var repository = [];
 
-// array of pokemon names
-var pokemonName = [
-  'Absol',
-  'Luxray',
-  'Pikachu',
-  'Milotic',
-  'Eevee',
-];
+ // array of pokemon names
+ var pokemonName = [
+   'Absol',
+   'Luxray',
+   'Pikachu',
+   'Milotic',
+   'Eevee',
+ ];
 
-// array of pokemon types, use array of strings
-var pokemonType = [
-  'water',
-  'electric',
-  'dark',
-  'normal'
-];
+ // array of pokemon types, use array of strings
+ var pokemonType = [
+   'water',
+   'electric',
+   'dark',
+   'normal'
+ ];
 
-// array of pokemon heights, use array of strings
-var pokemonHeight = [
-  "3'11\"",
-  "4'07\"",
-  "1'04\"",
-  "20'04\"",
-  "1'00\"",
-];
+ // array of pokemon heights, use array of strings
+ var pokemonHeight = [
+   "3'11\"",
+   "4'07\"",
+   "1'04\"",
+   "20'04\"",
+   "1'00\"",
+ ];
+
+ repository.push(pokemonName, pokemonType, pokemonHeight);
+
+ for (var i = 0; i < pokemonName.length; i++) {
+   document.write(pokemonName[i] + "<br>");
+ }
 
 repository.push(pokemonName, pokemonType, pokemonHeight);
 
