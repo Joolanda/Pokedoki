82d3e6af4c257c30ddbfadf7375030016651638f
Joolanda <50619917+Joolanda@users.noreply.github.com>
2019-11-20 18:32:28 +0100

    update task 1.3 script

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,63 @@
 /*!
- * Here comes all Javascript
- * Pokedoki App
- * Date: 2019-11-11
- */
+* Here comes all Javascript
+* Pokedoki App
+* Date: 2019-11-11
+*/
 
- // This array will become the repository of Pokémon to display in myapplication
-var repository = [];
+// This array will become the repository of Pokémon to display in myapplication
+const pokemon = [];
+// add several objects (Pokémons)to the array.
+// Each Pokémon needs to have the same keys to avoid errors
 
- // array of pokemon names
- var pokemonName = [
-   'Absol',
-   'Luxray',
-   'Pikachu',
-   'Milotic',
-   'Eevee',
- ];
+var absol = {
+  name:'Absol',
+  height: 1.2,
+  type: 'dark',
+};
 
- // array of pokemon types, use array of strings
- var pokemonType = [
-   'water',
-   'electric',
-   'dark',
-   'normal'
- ];
+var luxray = {
+  name:'Luxray',
+  height: 1.4,
+  type: 'electric',
+};
 
- // array of pokemon heights, use array of strings
- var pokemonHeight = [
-   1.2,
-   1.4,
-   0.4,
-   6.2,
-   0.3,
- ];
+var pikachu = {
+  name:'Pikachu',
+  height: 0.4,
+  type: 'electric',
+};
 
-for (var j = 0; j < pokemonHeight.length; j++) {
-var e = pokemonHeight;
- var result = e;
+var milotic = {
+  name: 'Milotic',
+  height: 6.2,
+  type: 'water',
+};
 
- if (pokemonHeight[j] > e[j]) {
-  result = 'big';
-} else {
-  result = 'little';
-}
-}
+var eevee = {
+  name:'Eevee',
+  height: 0.3,
+  type: 'normal',
+};
 
- repository.push(pokemonName, pokemonType, pokemonHeight);
+pokemon.push(absol);
+pokemon.push(luxray);
+pokemon.push(pikachu);
+pokemon.push(milotic);
+pokemon.push(eevee);
 
- for (var i = 0; i < pokemonName.length; i++) {
-   document.write(pokemonName[i]+" "+"(height: " +
-   pokemonHeight[i]+")" + " " + result + "<br>");
- }
+
+//var biggest = " Wow,that\'s big!"
+for (var i = 0; i < pokemon.length; i++){
+
+  var biggest = pokemon[i].height;
+  var result;
+  if (biggest <= 5) {
+  result = "\ ";
+  } else {
+  result = " Wow,that\'s big!";
+  }
+
+document.write("<br>" + pokemon[i].name  +" (" + "height: " +pokemon[i].height+")" + result  +  "<br>");
+// printing repository[i]’s other details
+  // ...
+};
