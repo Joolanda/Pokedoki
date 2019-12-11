5f1a70abc2f19a80c6dd284d2b9cc7c677ec79d0
Joolanda <50619917+Joolanda@users.noreply.github.com>
2019-12-11 14:52:34 +0100

    cut the code for new button into new addListItem function

--- js/scripts.js
+++ js/scripts.js
@@ -75,17 +75,6 @@ return { /*Return All Previous Function In Order To Be Available Outside Of IIFE
 //create a variable above the forEach loop block, then assign it the ul element
 var pokemonRepository = document.querySelector(".pokemon-list")
 
-pokemonRepository.getAll().forEach(function(pokemon){// create an Li element that contains a button for each pokemon,making sure to create a variable to hold each element of the two
-var $listItem = document.createElement("li");
-// append the 'li' list items along with all child elements into the 'ul' list
-$pokemonList.appendChild($listItem);
-
-var $button = document.createElement("button");
-//  append the button to the list item as its child.
-$listItem.appendChild($button);
-// set innertext of the button to be the Pokemon's name, remember that forEach returns a pokemon in each iteration
-$button.innerText = pokemon.name;
-$button.classList.add("list-button");
-$listItem.classList.add("buttonstyle");
-
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
  });
