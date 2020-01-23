f0f432d8d9d618c05d21ebd7460a2d9a44bf77bd
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-01-23 19:40:22 +0100

    writing js for registration form

---
+++ js/reg.validation.js
@@ -0,0 +1,26 @@
+(function() {
+  var $form = document.querySelector('#register-form');
+  var $emailInput = document.querySelector('#email');
+  var $passwordInput = document.querySelector('#password');
+
+  function validateEmail() {
+
+    return false;
+  }
+
+  function validatePassword() {
+
+    return false
+  }
+
+  function validateForm() {
+    return validateEmail() && validatePassword();
+  }
+
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault(); //Do not submit to the several
+    if (validateForm()) {
+      alert('Success!');
+    }
+  })
+})();
